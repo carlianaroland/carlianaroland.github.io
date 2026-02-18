9d68622d455b99ddc9dd5af24af18029ec3557a2
Carliana Roland <129014353+carlianaroland@users.noreply.github.com>
2026-02-17 22:26:18 -0500

    Refactor calculator to use template literals

--- calculator.js
+++ calculator.js
@@ -1,41 +1,40 @@
-let answers = [];
-
-document.write("<table>");
-document.write("<tr><th>Number 1</th><th>Operator</th><th>Number 2</th><th>Result</th></tr>");
+const answers = [];
+let tableRows = "";
 
 while (true) {
     let firstNum = prompt("First number: ");
-    if (firstNum == null) break; 
+    if (firstNum === null) break;
 
     let secNum = prompt("Second number: ");
-    if (secNum == null) break;
+    if (secNum === null) break;
 
     let opChoice = prompt("Choose Operator (+, -, *, /, %): ");
-    if (opChoice == null) break;
+    if (opChoice === null) break;
 
     let ans;
     let fNum = parseFloat(firstNum);
     let sNum = parseFloat(secNum);
 
-    if (isNaN(fNum) || isNaN(sNum)) { 
-        ans = "wrong input number"; 
+    if (isNaN(fNum) || isNaN(sNum)) {
+        ans = "wrong input number";
     } else {
-        if (opChoice == "+") ans = fNum + sNum;
-        else if (opChoice == "-") ans = fNum - sNum;
-        else if (opChoice == "*") ans = fNum * sNum;
-        else if (opChoice == "/") ans = fNum / sNum;
-        else if (opChoice == "%") ans = fNum % sNum;
+        if (opChoice === "+") ans = fNum + sNum;
+        else if (opChoice === "-") ans = fNum - sNum;
+        else if (opChoice === "*") ans = fNum * sNum;
+        else if (opChoice === "/") ans = fNum / sNum;
+        else if (opChoice === "%") ans = fNum % sNum;
         else ans = "computation error";
     }
 
-    if (typeof ans === "number") { 
-        answers.push(ans); 
-    }
+    if (typeof ans === "number") answers.push(ans);
+    tableRows += `<tr><td>${firstNum}</td><td>${opChoice}</td><td>${secNum}</td><td>${ans}</td></tr>`;
+}
 
-    document.write("<tr><td>"+firstNum+"</td><td>"+opChoice+"</td><td>"+secNum+"</td><td>"+ans+"</td></tr>");
-} 
-
-document.write("</table>");
+let html = `
+    <table>
+        <tr><th>Number 1</th><th>Operator</th><th>Number 2</th><th>Result</th></tr>
+        ${tableRows}
+    </table>`;
 
 if (answers.length > 0) {
     let min = Math.min(...answers);
@@ -43,10 +42,12 @@ if (answers.length > 0) {
     let total = answers.reduce((a, b) => a + b, 0);
     let avg = total / answers.length;
 
-    document.write("<h3>Summary Table</h3>");
-    document.write("<table>");
-    document.write("<table><tr><th>Minimum</th><th>Maximum</th><th>Average</th><th>Total</th></tr>");
-    document.write("<tr><td>"+min+"</td><td>"+max+"</td><td>"+avg.toFixed(2)+"</td><td>"+total+"</td></tr>");
-    document.write("</table>");
+    html += `
+        <h3>Summary Table</h3>
+        <table>
+            <tr><th>Minimum</th><th>Maximum</th><th>Average</th><th>Total</th></tr>
+            <tr><td>${min}</td><td>${max}</td><td>${avg.toFixed(2)}</td><td>${total}</td></tr>
+        </table>`;
 }
 
+document.getElementById("results").innerHTML = html;
